ef150cbb26573fcc3030dfde16485bf571efaa31
unknown <ghgarciar@gmail.com>
2023-08-22 23:47:20 -0300

    Menu in progress

--- lib/presentation/screens/login/users_screens.dart
+++ lib/presentation/screens/login/users_screens.dart
@@ -1,3 +1,4 @@
+import 'package:cefa_app/config/menu/menu_items.dart';
 import 'package:flutter/material.dart';
 
 class UsersScreen extends StatelessWidget {
@@ -6,7 +7,41 @@ class UsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('Inicio'))),
+        appBar: AppBar(title: const Center(child: Text('Inicio'))),
+        body: _HomeView());
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final MenuItem menuItem = appMenuItems[index];
+          return _CustomListTile(menuItem: menuItem);
+        });
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(menuItem.icon),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
     );
   }
 }
